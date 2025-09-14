d5765a2300c804f10b36e4a516ad507c52051405
Muhammad Abdullah Butt <140272393+AbdullahButt-00@users.noreply.github.com>
2025-09-14 23:53:22 +0500

    Create dataset_paths.py

---
+++ configs/dataset_paths.py
@@ -0,0 +1,20 @@
+dataset_paths = {
+    "Dataset_1": {
+        "train": "/content/drive/MyDrive/Dataset_1/d1_train.csv",
+        "test":  "/content/drive/MyDrive/Dataset_1/d1_test.csv"
+    },
+    "Dataset_2": {
+        "train": "/content/drive/MyDrive/Dataset_2/d2_train.csv",
+        "test":  "/content/drive/MyDrive/Dataset_2/d2_test.csv"
+    },
+    "Dataset_3": {
+        "train": "/content/drive/MyDrive/Dataset_3/d3_train.csv",
+        "test":  "/content/drive/MyDrive/Dataset_3/d3_test.csv"
+    },
+    "Dataset_4": {
+        "train": "/content/drive/MyDrive/Dataset_4/d4_train.csv",
+        "test":  "/content/drive/MyDrive/Dataset_4/d4_test.csv"
+    }
+}
+
+weight_base = "/content/drive/MyDrive/Model Weights Efficient-VIT : paper04"
