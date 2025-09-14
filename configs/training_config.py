9c16886193c49199a4a073d963f5ed920d929caa
Muhammad Abdullah Butt <140272393+AbdullahButt-00@users.noreply.github.com>
2025-09-14 23:53:41 +0500

    Add files via upload

---
+++ configs/training_config.py
@@ -0,0 +1,11 @@
+training_config = {
+    "num_classes": 3,
+    "batch_size": 20,
+    "num_epochs": 30,
+    "learning_rate": 1e-4,
+    "models": [
+        "efficientvit-b0", "efficientvit-b1", "efficientvit-b2", "efficientvit-b3",
+        "efficientvit-l1", "efficientvit-l2", "efficientvit-l3"
+    ]
+}
+
